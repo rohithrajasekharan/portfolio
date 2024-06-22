131a42bff88fd9003387b5d0890a00f0669297c7
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-06-22 18:01:35 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,6 +28,7 @@ export default function RootLayout({
           >
             {children}
             <SpeedInsights/>
+            <Analytics/>
           </ThemeProvider>
         </body>
     </html>
