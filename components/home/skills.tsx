dcb0a9c81362e1731ff99b1ee6f5987d5f65539b
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-07-02 07:14:49 +0530

    skills update

--- components/home/skills.tsx
+++ components/home/skills.tsx
@@ -6,27 +6,26 @@ export default function Skills() {
       <Card className="justify-between rounded-lg border p-4 space-y-8">
       <h2 className="text-xl font-semibold">Skills</h2>
         <div>
-          <h3 className="text-md font-semibold">HTML/CSS</h3>
+          <h3 className="text-md font-semibold">Go</h3>
           <Progress value={90} className="mt-2" />
           <p className="text-sm text-muted-foreground mt-2">
-            Proficient in writing semantic HTML and styling with CSS, including responsive design and modern CSS
-            techniques.
+            Proficient in writing server side code using go making full use of go routines and following the 
+            principles of effective go.
           </p>
         </div>
         <div>
-          <h3 className="text-md font-semibold">JavaScript</h3>
+          <h3 className="text-md font-semibold">Typescript</h3>
           <Progress value={85} className="mt-2" />
           <p className="text-sm text-muted-foreground mt-2">
-            Experienced in writing clean, efficient, and maintainable JavaScript code, including modern ES6+
-            features and frameworks/libraries like React.
+            Rooting for Type Safety across the industry by building projects making full use of type safe programming.
           </p>
         </div>
         <div>
           <h3 className="text-md font-semibold">React</h3>
           <Progress value={80} className="mt-2" />
           <p className="text-sm text-muted-foreground mt-2">
-            Skilled in building complex, responsive, and scalable web applications using React, including state
-            management, hooks, and modern best practices.
+            Most of my industrial experience lies in creating React applications for the airline industry. Have seen and moved along with
+            the evolution from class components to server side components.
           </p>
         </div>
         <div>
@@ -38,19 +37,18 @@ export default function Skills() {
           </p>
         </div>
         <div>
-          <h3 className="text-lg font-semibold">SQL</h3>
+          <h3 className="text-lg font-semibold">Next.js</h3>
           <Progress value={70} className="mt-2" />
           <p className="text-sm text-muted-foreground mt-2">
-            Proficient in writing complex SQL queries, understanding database design principles, and working with
-            relational databases like PostgreSQL and MySQL.
+            My favourite way of working with react applications that may or may not need SEO. Most of my side projects are done in 
+            Next.js
           </p>
         </div>
         <div>
-          <h3 className="text-lg font-semibold">Git</h3>
+          <h3 className="text-lg font-semibold">MongoDB / Sql</h3>
           <Progress value={90} className="mt-2" />
           <p className="text-sm text-muted-foreground mt-2">
-            Experienced in using Git for version control, including branching, merging, and collaborating with teams
-            on complex projects.
+            Worked with various Relational/Non-Relational databases and made use of popular ORMs. Also proficient in Graphql.
           </p>
         </div>
       </Card>
