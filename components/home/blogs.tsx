c174140651031960f4073ca0c22ab37d623e83b6
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-07-22 22:30:52 +0530

    added blog

--- components/home/blogs.tsx
+++ components/home/blogs.tsx
@@ -1,22 +1,24 @@
+import Link from "next/link";
+
 export default function Blogs(){
     return(
         <>
         <h2 className="text-xl font-semibold">Blogs</h2>
           <div className="space-y-8">
 
-            {/* <div className="grid gap-1">
-              <h3 className="font-semibold underline cursor-pointer">Scaling Rust Microservices with Kubernetes</h3>
+            <div className="grid gap-1">
+              <Link href="https://rohithrajasekharan.hashnode.dev/type-safety-across-the-stack-from-typescript-to-go" target="blank">
+              <h3 className="font-semibold underline cursor-pointer">Type Safety Across the Stack: From TypeScript to Go</h3>
+                </Link>
               <div className="flex items-center justify-between">
-                <p className="text-sm text-gray-500">Published on May 15, 2023</p>
+                <p className="text-sm text-gray-500">Published on Jul 22, 2024</p>
               </div>
               <p className="text-sm">
-                In this blog post, I share my experience in building and deploying a Rust-based microservices
-                architecture on Kubernetes, focusing on strategies for scalability, reliability, and performance
-                optimization.
+              This article shares personal experiences and insights on the benefits of type safety in software development, focusing on the transition from TypeScript to Go. Discover how strong typing can enhance code reliability, productivity, and overall developer satisfaction.
               </p>
             </div>
 
-            <div className="grid gap-1">
+            {/* <div className="grid gap-1">
               <h3 className="font-semibold underline cursor-pointer">Mastering TypeScript: A Beginner Guide</h3>
               <div className="flex items-center justify-between">
                 <p className="text-sm text-gray-500">Published on March 20, 2023</p>
--- components/home/socials.tsx
+++ components/home/socials.tsx
@@ -4,75 +4,75 @@ import Link from "next/link"
 
 export default function Socials() {
   return (
-      <div className="space-y-4 min-w-full">
-        <Card className="flex justify-between rounded-lg border p-4">
-          <div className="flex">
-            <TwitterIcon className="w-16 h-8 text-muted-foreground" />
-            <div>
-              <h3 className="text-lg font-medium">X</h3>
-              <Link href="https://x.com/dedsec4242" target="blank">
-              <p className="text-sm text-muted-foreground">Follow @dedsec4242</p>
-              </Link>
-            </div>
+    <div className="space-y-4 min-w-full">
+      <Card className="flex justify-between rounded-lg border p-4">
+        <div className="flex">
+          <TwitterIcon className="w-16 h-8 text-muted-foreground" />
+          <div>
+            <h3 className="text-lg font-medium">X</h3>
+            <p className="text-sm text-muted-foreground">Follow @dedsec4242</p>
           </div>
+        </div>
+        <Link href="https://x.com/dedsec4242" target="blank">
           <Button variant="outline" size="sm">
             Follow
           </Button>
-        </Card>
-        <Card className="flex justify-between rounded-lg border p-4">
-          <div className="flex">
-            <GithubIcon className="w-16 h-8 text-muted-foreground" />
-            <div>
-              <h3 className="text-lg font-medium">Github</h3>
-              <Link href="https://github.com/rohithrajasekharan" target="blank">
-              <p className="text-sm text-muted-foreground">Follow @rohithrajsekharan</p>
-              </Link>
-            </div>
+        </Link>
+      </Card>
+      <Card className="flex justify-between rounded-lg border p-4">
+        <div className="flex">
+          <GithubIcon className="w-16 h-8 text-muted-foreground" />
+          <div>
+            <h3 className="text-lg font-medium">Github</h3>
+            <p className="text-sm text-muted-foreground">Follow @rohithrajsekharan</p>
           </div>
+        </div>
+        <Link href="https://github.com/rohithrajasekharan" target="blank">
           <Button variant="outline" size="sm">
             Follow
           </Button>
-        </Card>
-        <Card className="flex justify-between rounded-lg border p-4">
-          <div className="flex">
-            <LinkedinIcon className="w-16 h-8 text-muted-foreground" />
-            <div>
-              <h3 className="text-lg font-medium">LinkedIn</h3>
-              <Link href="https://www.linkedin.com/in/rohithrajasekharan/" target="blank">
-              <p className="text-sm text-muted-foreground">Add to Network</p>
-              </Link>
-            </div>
+        </Link>
+      </Card>
+      <Card className="flex justify-between rounded-lg border p-4">
+        <div className="flex">
+          <LinkedinIcon className="w-16 h-8 text-muted-foreground" />
+          <div>
+            <h3 className="text-lg font-medium">LinkedIn</h3>
+            <p className="text-sm text-muted-foreground">Add to Network</p>
           </div>
+        </div>
+        <Link href="https://www.linkedin.com/in/rohithrajasekharan/" target="blank">
           <Button variant="outline" size="sm">
             Follow
           </Button>
-        </Card>
-        <Card className="flex justify-between rounded-lg border p-4">
-          <div className="flex">
-            <HashnodeIcon className="w-16 h-8 text-muted-foreground" />
-            <div>
-              <h3 className="text-lg font-medium">Hashnode</h3>
-              <Link href="https://rohithrajasekharan.hashnode.dev/" target="blank">
-              <p className="text-sm text-muted-foreground">Read blog posts</p>
-              </Link>
-            </div>
+        </Link>
+      </Card>
+      <Card className="flex justify-between rounded-lg border p-4">
+        <div className="flex">
+          <HashnodeIcon className="w-16 h-8 text-muted-foreground" />
+          <div>
+            <h3 className="text-lg font-medium">Hashnode</h3>
+            <p className="text-sm text-muted-foreground">Read blog posts</p>
           </div>
+        </div>
+        <Link href="https://rohithrajasekharan.hashnode.dev/" target="blank">
           <Button variant="outline" size="sm">
             Follow
           </Button>
-        </Card>
-      </div>
+        </Link>
+      </Card>
+    </div>
   )
 }
 
-function GithubIcon(props:any) {
+function GithubIcon(props: any) {
   return (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30"
-     fill="none" stroke="currentColor"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
+      fill="none" stroke="currentColor"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>
   )
 }
 
-function LinkedinIcon(props:any) {
+function LinkedinIcon(props: any) {
   return (
     <svg
       {...props}
@@ -89,22 +89,22 @@ function LinkedinIcon(props:any) {
 }
 
 
-function TwitterIcon(props:any) {
+function TwitterIcon(props: any) {
   return (
     <svg {...props}
-    fill="none"
-    stroke="currentColor" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
-<path d="M26.37,26l-8.795-12.822l0.015,0.012L25.52,4h-2.65l-6.46,7.48L11.28,4H4.33l8.211,11.971L12.54,15.97L3.88,26h2.65 l7.182-8.322L19.42,26H26.37z M10.23,6l12.34,18h-2.1L8.12,6H10.23z"></path>
-</svg>
+      fill="none"
+      stroke="currentColor" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
+      <path d="M26.37,26l-8.795-12.822l0.015,0.012L25.52,4h-2.65l-6.46,7.48L11.28,4H4.33l8.211,11.971L12.54,15.97L3.88,26h2.65 l7.182-8.322L19.42,26H26.37z M10.23,6l12.34,18h-2.1L8.12,6H10.23z"></path>
+    </svg>
   )
 }
 
 
-function HashnodeIcon(props:any) {
+function HashnodeIcon(props: any) {
   return (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48"fill="none"
-    stroke="currentColor">
-<path fill="none" d="M42.164,17.126L30.874,5.835c-3.781-3.781-9.967-3.781-13.748,0L5.836,17.126	c-3.781,3.781-3.781,9.967,0,13.748l11.291,11.291c3.781,3.781,9.967,3.781,13.748,0l11.291-11.291	C45.945,27.093,45.945,20.907,42.164,17.126z M24,31c-3.866,0-7-3.134-7-7c0-3.866,3.134-7,7-7s7,3.134,7,7	C31,27.866,27.866,31,24,31z"></path>
-</svg>
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48" fill="none"
+      stroke="currentColor">
+      <path fill="none" d="M42.164,17.126L30.874,5.835c-3.781-3.781-9.967-3.781-13.748,0L5.836,17.126	c-3.781,3.781-3.781,9.967,0,13.748l11.291,11.291c3.781,3.781,9.967,3.781,13.748,0l11.291-11.291	C45.945,27.093,45.945,20.907,42.164,17.126z M24,31c-3.866,0-7-3.134-7-7c0-3.866,3.134-7,7-7s7,3.134,7,7	C31,27.866,27.866,31,24,31z"></path>
+    </svg>
   )
 }
